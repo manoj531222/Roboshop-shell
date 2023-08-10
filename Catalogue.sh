850480e606f8f1bdfaede63d55ba863dae6117dc
manoj <gandhamanojkutty@gmail.com>
2023-08-10 22:22:49 +0530

    rm -rf /app added

--- Catalogue.sh
+++ Catalogue.sh
@@ -5,6 +5,7 @@ yum install nodejs -y
 echo -e "\e[36m>>>>>>>>> add roboshop user <<<<<<<<\e[0m"
 useradd roboshop
 echo -e "\e[36m>>>>>>>>> create app directory <<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
 echo -e "\e[36m>>>>>>>>> download app content <<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
@@ -13,7 +14,7 @@ unzip /tmp/catalogue.zip
 echo -e "\e[36m>>>>>>>>> install npm <<<<<<<<\e[0m"
 npm install
 echo -e "\e[36m>>>>>>>>> copy systemD content <<<<<<<<\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service
+cp catalogue.service /home/centos//etc/systemd/system/catalogue.service
 echo -e "\e[36m>>>>>>>>> daemon-reload <<<<<<<<\e[0m"
 systemctl daemon-reload
 echo -e "\e[36m>>>>>>>>> enable catalogue <<<<<<<<\e[0m"
