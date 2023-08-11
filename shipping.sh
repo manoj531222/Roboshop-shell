2ad60d86174baf90064b29fec7a945dd4965e642
manoj <gandhamanojkutty@gmail.com>
2023-08-11 18:58:11 +0530

    code updated

--- shipping.sh
+++ shipping.sh
@@ -1,11 +1,13 @@
 yum install maven -y
 useradd roboshop
+rm -rf /app
 mkdir /app
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
 cd /app
 unzip /tmp/shipping.zip
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
+cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
 systemctl daemon-reload
 systemctl enable shipping
 systemctl start shipping
