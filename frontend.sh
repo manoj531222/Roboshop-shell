dc4a616c2a9612531223243537d918c730350dc8
manoj <gandhamanojkutty@gmail.com>
2023-08-11 09:57:58 +0530

    code updated

--- frontend.sh
+++ frontend.sh
@@ -4,6 +4,5 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-systemctl start nginx
-systemctl enable nginx
 systemctl restart nginx
+systemctl enable nginx
