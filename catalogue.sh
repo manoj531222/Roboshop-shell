b4368f6ae65223766d46fc39af7c3c018811dc33
manoj <gandhamanojkutty@gmail.com>
2023-08-11 13:34:26 +0530

    code updated

--- catalogue.sh
+++ catalogue.sh
@@ -1,30 +1,15 @@
-echo -e "\e[36m>>>>>>>>> Configuring NodeJS repos <<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-echo -e "\e[36m>>>>>>>>> Install NodeJS <<<<<<<<\e[0m"
 yum install nodejs -y
-echo -e "\e[36m>>>>>>>>> Add Application User <<<<<<<<\e[0m"
 useradd roboshop
-echo -e "\e[36m>>>>>>>>> Create Application Directory <<<<<<<<\e[0m"
-rm -rf /app
 mkdir /app
-echo -e "\e[36m>>>>>>>>> Download App Content <<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
-echo -e "\e[36m>>>>>>>>> Unzip App Content <<<<<<<<\e[0m"
 unzip /tmp/catalogue.zip
-echo -e "\e[36m>>>>>>>>> Install NodeJS Dependencies <<<<<<<<\e[0m"
 npm install
-echo -e "\e[36m>>>>>>>>> Copy Catalogue SystemD file <<<<<<<<\e[0m"
-cp manoj531222/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
-echo -e "\e[36m>>>>>>>>> Start Catalogue Service <<<<<<<<\e[0m"
+cp catalogue.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
-
-echo -e "\e[36m>>>>>>>>> Copy MongoDB repo <<<<<<<<\e[0m"
-cp roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
-
-echo -e "\e[36m>>>>>>>>> Install MongoDB Client <<<<<<<<\e[0m"
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org-shell -y
-echo -e "\e[36m>>>>>>>>> Load Schema <<<<<<<<\e[0m"
 mongo --host mongodb-dev.mdevops333.online </app/schema/catalogue.js
