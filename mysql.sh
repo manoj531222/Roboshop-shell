043e0bece3f24d18d033ec86fb557a33cf27904c
manoj <gandhamanojkutty@gmail.com>
2023-08-11 19:33:41 +0530

    code updated

--- mysql.sh
+++ mysql.sh
@@ -2,5 +2,5 @@ dnf module disable mysql -y
 cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
 yum install mysql-community-server -y
 systemctl enable mysqld
-systemctl start mysqld
+systemctl restart mysqld
 mysql_secure_installation --set-root-pass RoboShop@1
