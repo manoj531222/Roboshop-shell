c1579a0ed09eb6f0db2120c61810a66a1e62589c
manoj <gandhamanojkutty@gmail.com>
2023-08-11 16:37:47 +0530

    redis code updated

--- Redis.sh
+++ Redis.sh
@@ -1,6 +1,6 @@
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 yum module enable redis:remi-6.2 -y
 yum install redis -y
-# need update ip 127.0.1.0 to 0.0.0.0
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
 systemctl enable redis
 systemctl start redis
