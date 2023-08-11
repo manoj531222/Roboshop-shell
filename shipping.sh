7700d4651ab7e1d60ac9b6d3a74d59150b843175
manoj <gandhamanojkutty@gmail.com>
2023-08-11 19:11:22 +0530

    code updated

--- shipping.sh
+++ shipping.sh
@@ -10,6 +10,7 @@ mv target/shipping-1.0.jar shipping.jar
 yum install mysql -y
 mysql -h mysql-dev.mdevops333.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
 cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
+echo """""""hellooooo"""""""""
 systemctl daemon-reload
 systemctl enable shipping
 systemctl restart shipping
