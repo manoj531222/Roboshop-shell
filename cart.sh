d95bbd2a94c91dd9d7320512474552765f79a0d8
manoj <gandhamanojkutty@gmail.com>
2023-08-11 18:12:34 +0530

    cart code updated

--- cart.sh
+++ cart.sh
@@ -7,7 +7,7 @@ curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
 unzip /tmp/cart.zip
 npm install
+cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service
 systemctl daemon-reload
 systemctl enable cart
 systemctl restart cart
-
