0f821de32f59f63a723f9e18c6d8df251095e5cc
manoj <gandhamanojkutty@gmail.com>
2023-08-11 18:47:51 +0530

    code updated

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,5 @@
 yum module disable mysql -y
-cp mysql.repo /etc/yum.repos.d/mysql.repo
-yum install mysql-community-server -y
+cp mysql.repo /home/centos/roboshop-shell/etc/yum.repos.d/mysql.repo
 yum install mysql-community-server -y
 systemctl enable mysqld
 systemctl start mysqld
