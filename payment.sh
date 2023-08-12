41b4540521b623e663274dc7df37bd918ddc0859
manoj <gandhamanojkutty@gmail.com>
2023-08-12 09:48:58 +0530

    code updated

--- payment.sh
+++ payment.sh
@@ -5,6 +5,7 @@ curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.
 cd /app
 unzip /tmp/payment.zip
 pip3.6 install -r requirements.txt
+cp /home/centos/roboshop-shell/payment.service /etc/systemd/system/payment.service
 systemctl daemon-reload
 systemctl enable payment
 systemctl start payment
