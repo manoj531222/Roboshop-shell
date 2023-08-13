eb611238767aeb53483a0a805783480e2f3f8be2
manoj <gandhamanojkutty@gmail.com>
2023-08-13 18:44:33 +0530

    payment

--- payment.sh
+++ payment.sh
@@ -1,5 +1,6 @@
 script_path=$(dirname $0)
 source ${script_path}/common.sh
+rabbitmq_appuser_password=$1
 yum install python36 gcc python3-devel -y
 useradd ${app_user}
 mkdir /app
@@ -7,6 +8,7 @@ curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.
 cd /app
 unzip /tmp/payment.zip
 pip3.6 install -r requirements.txt
+sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|" ${script_path}/payment.service
 cp ${script_path}/payment.service /etc/systemd/system/payment.service
 systemctl daemon-reload
 systemctl enable payment
