61fddb2647b9d9dc4c3fe4a8530c70f0d31d1df0
manoj <gandhamanojkutty@gmail.com>
2023-08-13 18:21:34 +0530

    variables

--- mysql.sh
+++ mysql.sh
@@ -1,8 +1,9 @@
 script_path=$(dirname $0)
+mysql_root_password=$1
 source ${script_path}/common.sh
 dnf module disable mysql -y
 cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo
 yum install mysql-community-server -y
 systemctl enable mysqld
 systemctl restart mysqld
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass ${mysql_root_password}
