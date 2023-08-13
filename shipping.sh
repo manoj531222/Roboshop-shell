e145cef7bf4e5c0ddcf8b706010e82c4014620a1
manoj <gandhamanojkutty@gmail.com>
2023-08-13 15:44:59 +0530

    script path

--- shipping.sh
+++ shipping.sh
@@ -1,4 +1,5 @@
-source common.sh
+script_path=$(dirname $0)
+source ${script_path}/common.sh
 echo -e "\e[36m>>>>>>>>> Install Maven <<<<<<<<\e[0m"
 yum install maven -y
 echo -e "\e[36m>>>>>>>>> Create App User <<<<<<<<\e[0m"
@@ -20,7 +21,7 @@ echo -e "\e[36m>>>>>>>>> Load Schema <<<<<<<<\e[0m"
 mysql -h mysql-dev.mdevops333.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
 
 echo -e "\e[36m>>>>>>>>> Setup SystemD Service <<<<<<<<\e[0m"
-cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
+cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
 
 echo -e "\e[36m>>>>>>>>> Start Shipping Service <<<<<<<<\e[0m"
 systemctl daemon-reload
