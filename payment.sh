00df5299e523d3e309bbfaaf8936b5fbc4fd626a
manoj <gandhamanojkutty@gmail.com>
2023-08-13 15:40:40 +0530

    script path

--- payment.sh
+++ payment.sh
@@ -1,4 +1,5 @@
-source common.sh
+script_path=$(dirname $0)
+source ${script_path}/common.sh
 yum install python36 gcc python3-devel -y
 useradd ${app_user}
 mkdir /app
@@ -6,7 +7,7 @@ curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.
 cd /app
 unzip /tmp/payment.zip
 pip3.6 install -r requirements.txt
-cp /home/centos/roboshop-shell/payment.service /etc/systemd/system/payment.service
+cp ${script_path}/payment.service /etc/systemd/system/payment.service
 systemctl daemon-reload
 systemctl enable payment
 systemctl start payment
