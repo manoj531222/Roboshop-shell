93c4e1cb6ceb386b2ad3505d175dcbad7fca0b37
manoj <gandhamanojkutty@gmail.com>
2023-08-14 10:51:31 +0530

    func_nodejs

--- user.sh
+++ user.sh
@@ -1,18 +1,7 @@
 script_path=$(dirname $0)
 source ${script_path}/common.sh
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd ${app_user}
-rm -rf /app
-mkdir /app
-curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
-cd /app
-unzip /tmp/user.zip
-npm install
-cp ${script_path}/user.service /etc/systemd/system/user.service
-systemctl daemon-reload
-systemctl enable user
-systemctl restart user
+component=user
+func_nodejs
 cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org-shell -y
 mongo --host mongodb-dev.mdevops333.online </app/schema/user.js
