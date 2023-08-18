b92d2e40ed95ac2c6e463dfa161f01f7428cffbf
manoj <gandhamanojkutty@gmail.com>
2023-08-18 15:29:24 +0530

    shell commands added

--- shipping.sh
+++ shipping.sh
@@ -4,7 +4,7 @@ source ${script_path}/common.sh
 mysql_root_password=$1
 if [ -z "$mysql_root_password" ]; then
   echo Input MySQL Root Password Missing
-  exit
+  exit 1
 fi
 
 component="shipping"
